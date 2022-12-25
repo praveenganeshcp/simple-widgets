298e173f5a3932ef0432cd9ef932c0fd05710706
praveenganeshcp <praveenganesh7@gmail.com>
2022-12-25 13:13:12 +0530

    feat(accordion): implement basic accordion without styles

---
+++ projects/ngx-simple-widgets/src/modules/accordion/components/sw-accordion/sw-accordion.component.ts
@@ -0,0 +1,26 @@
+import { CdkAccordionItem } from '@angular/cdk/accordion';
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'sw-accordion',
+  templateUrl: './sw-accordion.component.html',
+  styleUrls: ['./sw-accordion.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class SwAccordionComponent {
+
+    @ViewChild(CdkAccordionItem) cdkAccordionItem: CdkAccordionItem | undefined;
+
+    constructor() { }
+
+    toggle() {
+        if(this.cdkAccordionItem) {
+            this.cdkAccordionItem.toggle();
+        }
+    }
+
+    get isExpanded(): boolean {
+        return this.cdkAccordionItem ? this.cdkAccordionItem.expanded : false;
+    }
+
+}
