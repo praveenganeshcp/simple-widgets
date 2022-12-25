b5e89f0615b6d04088617ba3784a1733cad7197f
Praveen Kumar <praveenganesh7@gmail.com>
2022-12-25 14:12:13 +0530

    feat(accordion): implement basic accordion without styles

---
+++ projects/ngx-simple-widgets/src/modules/accordion/components/sw-accordion-title/sw-accordion-title.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'sw-accordion-title',
+  template: '<ng-content></ng-content>'
+})
+export class SwAccordionTitleComponent { }
--- projects/ngx-simple-widgets/src/public-api.ts
+++ projects/ngx-simple-widgets/src/public-api.ts
@@ -4,3 +4,8 @@
 
 export { SwIconModule } from "./modules/sw-icon/sw-icon.module";
 export { SwIconComponent } from "./modules/sw-icon/component/sw-icon/sw-icon.component";
+// accordion module
+export { SwAccordionModule } from "./modules/accordion/accordion.module";
+export { SwAccordionComponent } from "./modules/accordion/components/sw-accordion/sw-accordion.component";
+export { SwAccordionTitleComponent } from "./modules/accordion/components/sw-accordion-title/sw-accordion-title.component";
+export { SwAccordionBodyComponent } from "./modules/accordion/components/sw-accordion-body/sw-accordion-body.component";
