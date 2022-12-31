a216354a9003a74059fbe1e63275c1b9847deb3c
Praveen Kumar <praveenganesh7@gmail.com>
2022-12-31 08:18:49 +0530

    refactor(accordion): move code into sw-accordion root folder

---
+++ projects/ngx-simple-widgets/src/modules/sw-accordion/components/sw-accordion/sw-accordion.stories.ts
@@ -0,0 +1,7 @@
+import { Story, Meta } from "@storybook/angular";
+import { SwAccordionComponent } from "./sw-accordion.component";
+
+export default {
+    title: 'Accordion',
+    component: SwAccordionComponent
+} as Meta;
