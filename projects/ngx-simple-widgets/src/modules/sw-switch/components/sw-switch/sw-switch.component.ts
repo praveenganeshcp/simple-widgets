d7ce8022796e9b938644f869119b800fbb2149b0
Praveen Kumar <praveenganesh7@gmail.com>
2022-12-31 08:28:35 +0530

    feat(switch): implement basic switch component (#12)

---
+++ projects/ngx-simple-widgets/src/modules/sw-switch/components/sw-switch/sw-switch.component.ts
@@ -0,0 +1,61 @@
+import { Component, forwardRef } from "@angular/core";
+import { state, style, transition, animate, trigger } from "@angular/animations";
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from "@angular/forms";
+
+@Component({
+    selector: 'sw-switch',
+    templateUrl: './sw-switch.component.html',
+    styleUrls: ['./sw-switch.component.css'],
+    animations: [
+        trigger('switch-state', [
+            state('on', style({
+                transform: 'translate(20px, 0px)'
+            })),
+            state('off', style({
+                transform: 'translate(0px, 0px)'
+            })),
+            transition('on <=> off', [
+                animate('0.2s')
+            ])
+        ])
+    ],
+    providers: [     
+        {       
+            provide: NG_VALUE_ACCESSOR, 
+            useExisting: forwardRef(() => SwSwitchComponent),
+            multi: true     
+        }   
+    ]
+})
+export class SwSwitchComponent implements ControlValueAccessor {
+
+    state: boolean = false;
+
+    private onChange: Function | null = null;
+
+    private onTouched: Function | null = null;
+
+    writeValue(state: boolean): void {
+        this.state = state;
+    }
+
+    registerOnChange(onChange: Function): void {
+        this.onChange = onChange;
+    }
+
+    toggle() {
+        this.state = ! this.state;
+        if(this.onChange) {
+            this.onChange(this.state);
+        }
+    }
+
+    registerOnTouched(onTouched: Function): void {
+        this.onTouched = onTouched;
+    }
+
+    setDisabledState?(isDisabled: boolean): void {
+        console.log(isDisabled);
+    }
+    
+}
