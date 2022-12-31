848c7036475da54890c084d718ca6cf59cbfcb27
praveenganeshcp <praveenganesh7@gmail.com>
2022-12-31 07:53:58 +0530

    feat(icon): add basic story

--- projects/ngx-simple-widgets/src/public-api.ts
+++ projects/ngx-simple-widgets/src/public-api.ts
@@ -2,10 +2,13 @@
  * Public API Surface of ngx-simple-widgets
  */
 
+// icon module
 export { SwIconModule } from "./modules/sw-icon/sw-icon.module";
 export { SwIconComponent } from "./modules/sw-icon/component/sw-icon/sw-icon.component";
+
+
 // accordion module
-export { SwAccordionModule } from "./modules/accordion/accordion.module";
-export { SwAccordionComponent } from "./modules/accordion/components/sw-accordion/sw-accordion.component";
-export { SwAccordionTitleComponent } from "./modules/accordion/components/sw-accordion-title/sw-accordion-title.component";
-export { SwAccordionBodyComponent } from "./modules/accordion/components/sw-accordion-body/sw-accordion-body.component";
+export { SwAccordionModule } from "./modules/sw-accordion/sw-accordion.module";
+export { SwAccordionComponent } from "./modules/sw-accordion/components/sw-accordion/sw-accordion.component";
+export { SwAccordionTitleComponent } from "./modules/sw-accordion/components/sw-accordion-title/sw-accordion-title.component";
+export { SwAccordionBodyComponent } from "./modules/sw-accordion/components/sw-accordion-body/sw-accordion-body.component";
