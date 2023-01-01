a2cfdacc91e5dd645e1c9966e5fa2a4bcc818290
Praveen Kumar <praveenganesh7@gmail.com>
2023-01-01 18:52:29 +0530

    test(icon): add basic component testcases (#15)

--- projects/ngx-simple-widgets/src/modules/sw-icon/component/sw-icon/sw-icon.component.cy.ts
+++ projects/ngx-simple-widgets/src/modules/sw-icon/component/sw-icon/sw-icon.component.cy.ts
@@ -1,4 +1,26 @@
+import { SwIconComponent } from "./sw-icon.component";
+import { MatIconModule } from "@angular/material/icon";
 
 describe('SwIconComponent', () => {
-  
+
+    function mountComponent(iconName?: string) {
+        cy.mount(SwIconComponent, {
+            componentProperties: {
+                name: iconName
+            },
+            imports: [
+                MatIconModule
+            ]
+        })
+    }
+
+    it("should render home icon if 'home' is passed as icon name", () => {
+        mountComponent('home')
+        cy.get('mat-icon').should('have.text', 'home')
+    })
+
+    it('should not render the icon if name is not specified', () => {
+        mountComponent();
+        cy.get('mat-icon').should('have.text', '');
+    })
 });
