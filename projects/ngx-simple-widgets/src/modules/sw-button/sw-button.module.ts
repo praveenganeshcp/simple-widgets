edfb76a66c4e4387bda1fedf725420c2e91a0f12
praveenganeshcp <praveenganesh7@gmail.com>
2023-01-21 10:00:02 +0530

    feat(button): show loader till subscription is unsubscribed

--- projects/ngx-simple-widgets/src/modules/sw-button/sw-button.module.ts
+++ projects/ngx-simple-widgets/src/modules/sw-button/sw-button.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
 import { SwButtonComponent } from './components/sw-button/sw-button.component';
 
 @NgModule({
+  imports: [
+    CommonModule
+  ],
   declarations: [
     SwButtonComponent
   ],
