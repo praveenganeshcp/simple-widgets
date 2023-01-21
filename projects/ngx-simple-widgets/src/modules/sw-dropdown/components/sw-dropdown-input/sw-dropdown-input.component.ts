c3d8ae5925574ffdb660ba135441df709809dc90
praveenganeshcp <praveenganesh7@gmail.com>
2023-01-21 16:45:01 +0530

    feat(dropdown): implement control value accessor for dropdown

--- projects/ngx-simple-widgets/src/modules/sw-dropdown/components/sw-dropdown-input/sw-dropdown-input.component.ts
+++ projects/ngx-simple-widgets/src/modules/sw-dropdown/components/sw-dropdown-input/sw-dropdown-input.component.ts
@@ -1,13 +1,21 @@
 import { ListboxValueChangeEvent } from '@angular/cdk/listbox';
-import { Component, Input } from '@angular/core';
+import { Component, forwardRef, Input } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { KeyValuePair } from '../../sw-dropdown.types';
 
 @Component({
   selector: 'sw-dropdown-input',
   templateUrl: './sw-dropdown-input.component.html',
-  styleUrls: ['./sw-dropdown-input.component.scss']
+  styleUrls: ['./sw-dropdown-input.component.scss'],
+  providers: [
+    {
+        provide: NG_VALUE_ACCESSOR,
+        useExisting: forwardRef(() => SwDropdownInputComponent),
+        multi: true        
+    }
+  ]
 })
-export class SwDropdownInputComponent {
+export class SwDropdownInputComponent implements ControlValueAccessor {
 
   @Input() items: KeyValuePair[] = [];
 
@@ -15,16 +23,42 @@ export class SwDropdownInputComponent {
 
   isOpen: boolean = false;
 
-  selectedValue: unknown[] = [];
+  selectedValues: string[] = [];
+
+  onChangeFn: Function | null = null;
+
+  onTouchFn: Function | null = null;
 
   get selectedValueVM() {
-    return this.selectedValue.join(', ');
+    return this.selectedValues ? this.selectedValues.join(', ') : '';
   }
 
   constructor() { }
 
-  onValueChange(event: ListboxValueChangeEvent<unknown>) {
-    this.selectedValue = [event.value[0]];
+  writeValue(value: string[]): void {
+    this.selectedValues = value;
+  }
+    
+  registerOnChange(fn: Function): void {
+    this.onChangeFn = fn;
+  }
+
+  onUpdateValue(updatedValue: string[]) {
+    this.selectedValues = updatedValue;
+    if(this.onChangeFn) {
+        this.onChangeFn(this.selectedValues);
+    }
+  }
+
+  registerOnTouched(fn: Function): void {
+    this.onTouchFn = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+  }
+
+  onValueChange(event: ListboxValueChangeEvent<string>) {
+    this.onUpdateValue(event.value as string[]);
     this.isOpen = false;
   }
 
--- projects/ngx-simple-widgets/src/modules/sw-dropdown/sw-dropdown.types.ts
+++ projects/ngx-simple-widgets/src/modules/sw-dropdown/sw-dropdown.types.ts
@@ -1,4 +1,4 @@
 export interface KeyValuePair {
     key: string;
-    value: unknown
+    value: string;
 }
