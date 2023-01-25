bb7b9c6b24689b3421e9a6b7a5c77feef4b4b705
praveenganeshcp <praveenganesh7@gmail.com>
2023-01-25 06:18:22 +0530

    feat(icon): allow size to be configurable

--- projects/ngx-simple-widgets/src/modules/sw-icon/component/sw-icon/sw-icon.stories.ts
+++ projects/ngx-simple-widgets/src/modules/sw-icon/component/sw-icon/sw-icon.stories.ts
@@ -1,15 +1,32 @@
 import { Meta, Story, moduleMetadata } from "@storybook/angular";
 import { MatIconModule } from "@angular/material/icon";
 import { SwIconComponent } from "./sw-icon.component";
+import { DEFAULT_SW_ICON_SIZE } from "../../sw-icon.constants";
 
 export default {
-    title: 'icon',
+    title: 'Simple Widgets/Icon',
     component: SwIconComponent,
     decorators: [
         moduleMetadata({
             imports: [MatIconModule]
         })
-    ]
+    ],
+    argTypes: {
+        size: {
+            control: 'inline-radio',
+            options: ['small', 'medium', 'large'],
+            name: 'Size',
+            description: 'Size of the icon',
+            table: {
+                defaultValue: { summary: DEFAULT_SW_ICON_SIZE }
+            },
+        },
+        name: {
+            control: 'text',
+            name: 'Icon name',
+            description: 'Name of the icon. NOTE: Only material icons names are valid'
+        }
+    },
 } as Meta;
 
 const IconTemplate: Story = (args) => ({
@@ -21,5 +38,6 @@ const IconTemplate: Story = (args) => ({
 export const HomeIcon = IconTemplate.bind({});
 
 HomeIcon.args = {
-    name: 'home'
+    name: 'home',
+    size: DEFAULT_SW_ICON_SIZE
 }
---
+++ projects/ngx-simple-widgets/src/modules/sw-icon/sw-icon.constants.ts
@@ -0,0 +1,7 @@
+export enum SwIconSize {
+    small = "small",
+    medium = "medium",
+    large = "large"
+}
+
+export const DEFAULT_SW_ICON_SIZE: SwIconSize = SwIconSize.medium;
